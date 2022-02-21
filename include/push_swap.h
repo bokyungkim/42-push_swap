04c04342e77bf91f7050cef2e40acf9812fde31e
bokyungkim <ohholyday@naver.com>
2022-02-22 03:20:14 +0900

    added macros

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: bokim <bokim@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/15 18:33:01 by bokim             #+#    #+#             */
-/*   Updated: 2022/02/22 03:12:00 by bokim            ###   ########.fr       */
+/*   Updated: 2022/02/22 03:18:20 by bokim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,18 @@
 # define FAIL	0
 # define TRUE	1
 # define FALSE	0
+
+# define SA	1
+# define SB	2
+# define SS	3
+# define PA	4
+# define PB	5
+# define RA	6
+# define RB	7
+# define RR	8
+# define RRA	9
+# define RRB	10
+# define RRR	11
 
 typedef struct s_node {
 	int				value;
